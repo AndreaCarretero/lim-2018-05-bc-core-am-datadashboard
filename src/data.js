44652e6becad51b9afa272a0832129b96559279f
LesAndreaCarretero <andrea.carretero2097@gmail.com>
2018-07-11 18:02:10 -0500

    Main terminado - figura en consola users- progress

--- src/data.js
+++ src/data.js
@@ -1,12 +1,139 @@
 window.computeUsersStats = (users, progress, courses) => {
+  /* let user1 = users;
+  let progress1= progress;
 
+ 1hora y 20min...
+
+
+
+
+
+  let newUsers= users1.filter((user)=> user.role === 'student').map(user =>{
+    const progresYusuario = progress1[user.id];
+    return ({
+      stats:{
+        name: user.name,
+        percent:
+        exercises:{
+          total:
+          completed:
+          percent:
+        },
+        reads:{
+          total:
+          completed:
+          percent:          
+        },
+        quizzes:{
+          total:
+          completed:
+          percent: 
+          scoreSum:
+          scoreAvg:
+        }
+      }
+
+    })
+  }) */
+  // me retora nuevo array con usuarios
+
+  let newUsers= users1.map((user)=> user.role === 'student' { // me retora nuevo array con usuarios
+
+    /*  Declaro variables para cada propiedad del progreso de las alumnas
+     propiedades del progreso  */
+  let percent= 0;
+  let exerciseTotal= 0;
+  let exerciseCompleted= 0;
+  let readsTotal =0;
+  let readsCompleted= 0;
+  let quizzesTotal= 0;
+  let quizzesCompleted= 0;
+  let scoreSum= 0;
+  let scoreAvg= 0;
+
+  // Paso 1: calcúlo porcentaje de completitud para cada usuario --> courses
+
+  courses.forEach(coursesName => {// recorriendo nombre de los cursos
+
+    if(progress[user.id].hasOwnProperty([coursesName])){// hasOwnProperty me indica si el usuario cuenta con la propiedad que estoy evaluando y devuelve un booleano
+      percent= progress[user.id].intro.percent; 
+      const usersUnits= progress[user.id].intro.units;
+      Object.keys(usersUnits).forEach((unitName)=>{// Se capturan los arrays con las propiedades enumerdas  y las recorro con el método de forEach
+        const parts= usersUnits[unitName].parts
+        Object.keys(parts).forEach((partName)=>{
+          const partsName= parts[partName];
+          console.log(partsName);
+          if(partsName.hasOwnProperty('exercises')){
+            const exercises= partName.exercises;
+            Object.keys(exercises).forEach((exerciseName)=>{
+              const exercise= exercises[exerciseName];
+              exerciseTotal += 1;
+              if(exercise.completed !== undefined){ // Se coloca undefined para que 
+                exerciseCompleted += exercise.completed;
+              }else{
+                exerciseCompleted = 0;
+              }
+            });
+          }
+          //calculo lecturas 
+
+          if(partsName.hasOwnProperty('type')){
+
+            if(partsName.type === 'reads'){
+              readsTotal++;
+              readsCompleted += partsName.completed;
+            }
+          }
+          //quizzes
+          if(partsName.hasOwnProperty('type')){
+            if(partsName.type === 'quiz'){
+              quizzesTotal+= 1;
+              quizzesTotal+= partsName.completed;
+              scoreSum+= partsName.score ? partsName.score : 0;
+              scoreAvg+= scoreSum / quizzesCompleted ? scoreSum / quizzesCompleted : 0;
+            }
+          }
+        })
+      })
+    }  
+  })
+  //caculo porcentaje
+  const exercisePercent= (exerciseCompleted / exerciseTotal)* 100
+  const readsPercent = (readsCompleted / readsTotal)* 100
+  const quizzesPercent = (quizzesCompleted/ quizzesTotal) *100
+
+// obtengo usuarias con sus progresos
+
+  const userWithSats ={
+    name: user.name.toUpperCase(),
+    stats:{
+      percent:percent,
+      exercises: {
+        total: exerciseTotal,
+        completed: exerciseCompleted,
+        percent : exercisePercent,
+      },
+      reads: {
+        total: readsTotal,
+        completed: readsCompleted,
+        percent : Math.round(readsPercent), //aquí se redondea 
+      },
+      quizzes: {
+        total: quizzesTotal,
+        completed: quizzesCompleted,
+        percent: Math.round(quizzesPercent),
+        scoreSum: Math.round(scoreSum),
+        scoreAvg: Math.round(scoreAvg)
+      }
+    }
+  }
+  return userWithSats
+  });
+  return newUsers
 };
-window.sortUsers = (users, orderBy, orderDirection) => {
 
-}
-window.filterUsers = (users, search) => {
-
-}
-window.processCohortData = (options) => {
-
+window.processCohortData =(options)=>{
+  let courses = Object.keys(options.cohort.coursesIndex);
+  let estudiantes = window.computeUsersStats(options.cohortData.users,options.cohortData.progress, courses )
+  console.log(courses);
 }
--- src/main.js
+++ src/main.js
@@ -1,147 +1,69 @@
 const listVenues = document.querySelector('#venues');
 const mainSection = document.getElementById('cohorts');
+const viewUsers = document.getElementById('users');
+const content = document.getElementById('selectsede');
 
+//Objeto "options" :
+let options = {
+  cohort: 0,
+  cohortData: {
+    users: null,
+    progress: null,
+  },
+  orderBy: 'name',
+  orderDirection: 'ASC',
+  search: ''
+}
 
 const getData = (str, url, callback) => {
-<<<<<<< HEAD
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
   xhr.addEventListener('load', event => {
-    if (event.target.readyState === 4) {
-      if (event.target.status !== 200) {
-        return console.error(new Error(`HTTP error: ${event.target.status}`))
-      } else {
-        const response = JSON.parse(event.target.responseText);
-        callback(str, response);
-      }
+    if (event.target.readyState === 4 && event.target.status === 200) {
+      const response = (JSON.parse(event.target.responseText));
+      callback(str, response);
     }
   });
   xhr.send();
-};
-
-
+}
+// 3ero
 const showCohorts = (city, dataCohorts) => {
-
-  
-  
+  options.cohort = dataCohorts;
   const cohortByCity = dataCohorts.filter(cohort => {
     return cohort.id.indexOf(city) !== -1;
   })
-  for (const cohort of cohortByCity) {
-    mainSection.innerHTML += `
-  <div>
-  <div id='listcohorts'>${cohort.id}</div>
-  </div>
-  `
-  }
-
-
-
+ content.innerHTML = '';
+ for(cohort of cohortByCity){
+    content.innerHTML +=
+      `<li id="${cohort.id}">${cohort.id}</li>`
+  };
 };
+const showProgress = (idOfCohorts, progressObject) => {
+  console.log(idOfCohorts, progressObject)
+  console.log(processCohortData(options))
+  options.cohortData.progress = progressObject;
+}
+//5to
 
+const showUsers = (idOfCohorts, usersArray) => {
+  options.cohortData.users = usersArray;
+  getData(idOfCohorts,`../data/cohorts/${idOfCohorts}/progress.json`, showProgress);
+}
+
+// Click
+
+//2do
 listVenues.addEventListener('click', event => {
-     // AL FIN , en una línea -Se limpia todo cuando el usuario dé click.
-    mainSection.innerHTML='';
   getData(event.target.id, '../data/cohorts.json', showCohorts);
 });
 
-let options = {
-    cohort: null,
-    cohortData: {
-      users: null,
-      progress: null,
-  
-    },
-    orderBy: 'Name',
-    orderDirection: 'ASC',
-    search: ''
-  }
-/* listVenues.addEventListener('click', event => {
-  mainSection.innerHTML = `
-    <div>
-        <p>${event.target.textContent}</p>
-    </div>
-    `
-});
- */
-/*
-let progress = {};
-let users = [];
-let cohorts = {};
+// 4to
+content.addEventListener('click', (event) => {
+     options.cohort.forEach((elementOfCohort) => { 
+      if (elementOfCohort.id === event.target.id){ 
+          options.cohort= elementOfCohort;
+      }
+  }); 
 
-const urlP = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
-const urlU = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-const urlC = '../data/cohorts.json';
-
-fetch(urlP).then((resp) => {
-    if (resp.status === 200) {
-        return resp.json();
-    } else {
-        console.log('ocurrió un error')
-    }
-}).then((respP) => {
-    progress = respP;
-    return fetch(urlU)
-}).then((resp) => {
-    if (resp.status === 200) {
-        return resp.json();
-    } else {
-        console.log('ocurrió un error')
-    }
-}).then((respU) => {
-    users = respU;
-    return fetch(urlC)
-}).then((resp) => {
-    if (resp.status === 200) {
-        return resp.json();
-    } else {
-        console.log('ocurrió un error')
-    }
-}).then((respC) => {
-    cohorts = respC;
-    computeUsersStats(users, progress, cohorts);
-
-
-})
-
-/*
-const getData = (url, callback) => {
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = () => {
-        if (request.readyState == 4 && request.status == 200) {
-            const reqObj = JSON.parse(request.response);
-            callback(reqObj);
-=======
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.addEventListener('load', event => {
-        if (event.target.readyState === 4) {
-            if (event.target.status !== 200) {
-                return console.error(new Error(`HTTP error: ${event.target.status}`))
-            } else {
-                const response = JSON.parse(event.target.responseText);
-                callback(str, response);
-            }
->>>>>>> 1b12252e41cc09920cd957c41a3408692d98a50b
-        }
-    });
-    xhr.send();
-};
-
-
-const showCohorts = (city, dataCohorts) => {
-    mainSection.innerHTML = `<select class="form-control" id="select"></select>`
-    const cohortByCity = dataCohorts.filter(cohort => {
-        return cohort.id.indexOf(city) !== -1;
-    })
-    cohortByCity.forEach(cohort => {
-        const options = document.getElementById('select')
-        options.innerHTML +=
-            `<option>${cohort.id}</option>`
-    });
-};
-
-listVenues.addEventListener('click', event => {
-    mainSection.innerHTML = '';
-    getData(event.target.id, '../data/cohorts.json', showCohorts);
+getData(event.target.id, `../data/cohorts/${event.target.id}/users.json`, showUsers)
 });
